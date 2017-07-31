6e09f44d45d7497b81f130aedae3389579bb4f91
liuke2015 <376060613@qq.com>
2017-07-31 15:06:41 +0800

    lesson11--map、set与数据和对象的比较

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -8703,188 +8703,110 @@
 
 	'use strict';
 
-	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
-	/**
-	 * Created by Administrator on 2017/7/31 0031.
-	 */
-	/*一、set*/
+	/*一、map与数组的比较*/
 	{
-	    /*1、定义--不加参数*/
-	    var list = new Set();
-	    list.add(5);
-	    list.add(7);
-	    console.log('list', list);
-	    console.log('size', list.size);
-	}
-
-	{
-	    /*2、定义--加参数*/
-	    var arr = [1, 2, 3, 4, 5];
-	    var _list = new Set(arr);
-
-	    console.log('size', _list.size);
-	}
-
-	{
-	    /*3、特性--元素不能重复*/
-	    var _list2 = new Set();
-	    _list2.add(1);
-	    _list2.add(2);
-	    _list2.add(1);
-
-	    console.log('list', _list2);
-
-	    var _arr = [1, 2, 3, 1, '2'];
-	    var list2 = new Set(_arr);
-
-	    console.log('unique', list2);
-	}
-
-	{
-	    /*4、方法--'add','delete','clear','has'*/
-	    var _arr2 = ['add', 'delete', 'clear', 'has'];
-	    var _list3 = new Set(_arr2);
-
-	    console.log('has', _list3.has('add'));
-	    console.log('delete', _list3.delete('add'), _list3);
-	    _list3.clear();
-	    console.log('list', _list3);
-	}
-
-	{
-	    /*5、遍历*/
-	    var _arr3 = ['add', 'delete', 'clear', 'has'];
-	    var _list4 = new Set(_arr3);
-
-	    var _iteratorNormalCompletion = true;
-	    var _didIteratorError = false;
-	    var _iteratorError = undefined;
-
-	    try {
-	        for (var _iterator = _list4.keys()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-	            var key = _step.value;
-
-	            console.log('keys', key);
-	        }
-	    } catch (err) {
-	        _didIteratorError = true;
-	        _iteratorError = err;
-	    } finally {
-	        try {
-	            if (!_iteratorNormalCompletion && _iterator.return) {
-	                _iterator.return();
-	            }
-	        } finally {
-	            if (_didIteratorError) {
-	                throw _iteratorError;
-	            }
-	        }
-	    }
-
-	    var _iteratorNormalCompletion2 = true;
-	    var _didIteratorError2 = false;
-	    var _iteratorError2 = undefined;
-
-	    try {
-	        for (var _iterator2 = _list4.values()[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-	            var value = _step2.value;
-
-	            console.log('value', value);
-	        }
-	    } catch (err) {
-	        _didIteratorError2 = true;
-	        _iteratorError2 = err;
-	    } finally {
-	        try {
-	            if (!_iteratorNormalCompletion2 && _iterator2.return) {
-	                _iterator2.return();
-	            }
-	        } finally {
-	            if (_didIteratorError2) {
-	                throw _iteratorError2;
-	            }
-	        }
-	    }
-
-	    var _iteratorNormalCompletion3 = true;
-	    var _didIteratorError3 = false;
-	    var _iteratorError3 = undefined;
-
-	    try {
-	        for (var _iterator3 = _list4.entries()[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-	            var _step3$value = _slicedToArray(_step3.value, 2),
-	                _key = _step3$value[0],
-	                _value = _step3$value[1];
-
-	            console.log('entries', _key, _value);
-	        }
-	    } catch (err) {
-	        _didIteratorError3 = true;
-	        _iteratorError3 = err;
-	    } finally {
-	        try {
-	            if (!_iteratorNormalCompletion3 && _iterator3.return) {
-	                _iterator3.return();
-	            }
-	        } finally {
-	            if (_didIteratorError3) {
-	                throw _iteratorError3;
-	            }
-	        }
-	    }
-
-	    _list4.forEach(function (item) {
-	        console.log(item);
-	    });
-	}
-
-	/*二、WeakSet*/
-	/*WeakSet与Set的区别-- 支持类型不一样
-	*   WeakSet的key值只支持对象，且对象是弱引用--地址引用。
-	*   没有clear方法，没有size属性,不能遍历
-	* */
-
-	{
-	    var weakList = new WeakSet();
-
-	    var arg = {};
-
-	    weakList.add(arg);
-
-	    // weakList.add(2);
-
-	    console.log('weakList', weakList);
-	}
-	/*三、map*/
-	/*map的key可以是任何类型，甚至是数组类型*/
-	{
+	    //数据结构横向对比，增、查、改、删
 	    var map = new Map();
-	    var _arr4 = ['123'];
+	    var array = [];
+	    //增
+	    map.set('t', 1);
+	    array.push({ 't': 1 });
 
-	    map.set(_arr4, 456);
+	    console.info('map-array', map, array);
 
-	    console.log('map', map, map.get(_arr4));
+	    //查
+	    var map_exist = map.has('t');
+	    var array_exist = array.find(function (item) {
+	        return item.t;
+	    });
+	    console.info('map-array', map_exist, array_exist);
+
+	    //改
+	    map.set('t', 2);
+	    array.forEach(function (item) {
+	        return item.t ? item.t = 2 : '';
+	    });
+	    console.info('map-array-modify', map, array);
+
+	    //删
+	    map.delete('t');
+	    var index = array.findIndex(function (item) {
+	        return item.t;
+	    });
+	    array.splice(index, 1);
+	    console.info('map-array-empty', map, array);
 	}
 
+	/*二、set与数组的比较*/
 	{
-	    var _map = new Map([['a', 123], ['b', 456]]);
-	    console.log('map args', _map);
-	    console.log('size', _map.size);
-	    console.log('delete', _map.delete('a'), _map);
-	    console.log('clear', _map.clear(), _map);
-	}
-	/*四、WeakMap*/
-	/*   WeakMap的key值只支持对象，且对象是弱引用--地址引用。
-	 *   没有clear方法，没有size属性,不能遍历
-	*
-	* */
-	{
-	    var weakmap = new WeakMap();
+	    var set = new Set();
+	    var _array = [];
 
-	    var o = {};
-	    weakmap.set(o, 123);
-	    console.log(weakmap.get(o));
+	    //增
+	    set.add({ t: 1 });
+	    _array.push({ t: 1 });
+
+	    console.info("set-array1", set, _array);
+
+	    //查
+	    var set_exist = set.has({ t: 1 }); //false
+	    var _array_exist = _array.find(function (item) {
+	        return item.t;
+	    });
+	    console.info('set-array2', set_exist, _array_exist);
+
+	    //改
+	    set.forEach(function (item) {
+	        return item.t ? item.t = 2 : '';
+	    });
+	    _array.forEach(function (item) {
+	        return item.t ? item.t = 2 : '';
+	    });
+	    console.info('set-array-modify', set, _array);
+
+	    //删
+	    set.forEach(function (item) {
+	        return item.t ? set.delete(item) : '';
+	    });
+	    var _index = _array.findIndex(function (item) {
+	        return item.t;
+	    });
+	    _array.splice(_index, 1);
+	    console.info('set-array-empty', set, _array);
+	}
+
+	/*三、map,set与Object的对比*/
+	{
+	    var item = { t: 1 };
+	    var _map = new Map();
+	    var _set = new Set();
+	    var obj = {};
+
+	    //增加
+	    _map.set('t', 1);
+	    _set.add(item);
+	    obj['t'] = 1;
+
+	    console.info('map-set-obj', obj, _map, _set);
+
+	    //查
+	    console.info({
+	        map_exist: _map.has('t'),
+	        set_exist: _set.has(item),
+	        obj_exist: 't' in obj
+	    });
+
+	    //改
+	    _map.set('t', 2);
+	    item.t = 2;
+	    obj['t'] = 2;
+	    console.info('map-set-obj-modify', obj, _map, _set);
+
+	    //删
+	    _map.delete('t');
+	    _set.delete(item);
+	    delete obj['t'];
+	    console.info('map-set-obj-empty', obj, _map, _set);
 	}
 
 /***/ })
