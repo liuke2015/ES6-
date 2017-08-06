c8b3a1fb85a9c5b861d6d9f8e46a17a13d8ab365
liuke2015 <376060613@qq.com>
2017-08-06 11:36:55 +0800

    彩票项目实战模块搭建完成

--- app/js/index.js
+++ app/js/index.js
@@ -1,14 +1,15 @@
 /*import './class/lesson1';*/
-/*
-import './class/lesson2';*/
-/*import "babel-polyfill";*/
+
 
 //import {A,test,Hello} from './class/lesson18';
 //import {A} from './class/lesson18';
 //import * as lesson from './class/lesson18'//as后面取一个别名
-import lesson18 from './class/lesson18'
+//import lesson18 from './class/lesson18'
 
 //console.info(A,test,Hello);
 //console.info(A);
 //console.log(lesson.A,lesson.test);
-console.log(lesson18.A);
+//console.log(lesson18.A);
+
+import "babel-polyfill";
+import lottery from './lottery'
---
+++ app/js/lottery.js
@@ -0,0 +1,7 @@
+/**
+ * Created by dell on 2017/8/6.
+ */
+import './lottery/base.js';
+import './lottery/timer.js';
+import './lottery/calculate.js';
+import './lottery/interface.js';
